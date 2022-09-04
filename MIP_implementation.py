3e805fca01312adcd565d9c1fccf87fada2a5133
Oscar Davine <oscardavine@gmail.com>
2022-09-04 21:57:38 +1000

    peter additions

--- MIP_implementation.py
+++ MIP_implementation.py
@@ -1,18 +1,19 @@
 import gurobipy as gp
 import math
 import matplotlib.pyplot as plt
-
+from matplotlib.path import Path
+import matplotlib.patches as patches
 # data
 # P-n16-k8 augerat problem
 N= {
-"n0 ": (37, 52),
-"n1 ": (49, 49),
-"n2 ": (52, 64),
-"n3 ": (31, 62),
-"n4 ": (52, 33),
-"n5 ": (42, 41),
-"n6 ": (52, 41),
-"n7 ": (57, 58),
+"n0": (37, 52),
+"n1": (49, 49),
+"n2": (52, 64),
+"n3": (31, 62),
+"n4": (52, 33),
+"n5": (42, 41),
+"n6": (52, 41),
+"n7": (57, 58),
 "n8": (62, 42),
 "n9": (42, 57),
 "n10": (27, 68),
@@ -35,17 +36,20 @@ N_st['t'] = t
 
 # default data
 drone_num = 2
-drone_speeds = [1, 1.5]
+drone_speeds = 2.5#[1, 1.5]
+a = [1, 1.5]
 drone_battery = B_l = [100, 100] # max flight time
 truck_speed = 1
 truck_service_time = 10
 drone_flight_duration = 100
 drone_service_time = 5
-M = 1000000
-L = range(len(drone_speeds))
+# M = 1000000
+L = range(len(a))
 
 # generate arcs
-A = [(i,j) for i in N_s for j in N_t if i != j]
+A = [(i,j) for i in N_s for j in N_t if N_s[i] != N_t[j]]
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+M = len(A)+1
 
 # generate arc distances
 def generate_distances(all_arcs):
@@ -59,14 +63,14 @@ def dist_betw_arcs(arc1, arc2):
     """generates distance between arcs"""
     return math.sqrt((arc1[0] - arc2[0])**2 + (arc1[1] - arc2[1])**2)
 
-# generate truck wait time
-T_v = {(i,j): truck_service_time + dist_betw_arcs(N_st[i], N_st[j])/truck_speed for (i,j) in A}
+# generate truck wait time!!!!!!!!!!!!!!!!!!!!!
+T_v = {(i,j): truck_service_time + generate_distances(A)[A.index((i,j))]/truck_speed for (i,j) in A}
 
-# generate drone delivery times (battery usage), includes service time 
-b_l = {(i,j,l): (generate_distances(A)[A.index((i,j))]*2) + drone_service_time for i in N_s for j in N for l in L if i != j}
+# generate drone delivery times (battery usage), includes service time !!!!!!!!!!!!!!!!!
+b_l = {(i,j,l): a[l]*((generate_distances(A)[A.index((i,j))]*2/drone_speeds) + drone_service_time) for i in N_s for j in N for l in L if i != j}
 
 # total required ddelivery time
-tau_l = {(i,j,l): (generate_distances(A)[A.index((i,j))]*2)/drone_speeds[l] + drone_service_time for i in N_s for j in N for l in L if i != j}
+tau_l = {(i,j,l): (generate_distances(A)[A.index((i,j))]*2)/(drone_speeds*a[l]) + drone_service_time for i in N_s for j in N for l in L if i != j}
 
 
 # s is first node (source depot)
@@ -77,7 +81,7 @@ m = gp.Model("MIP_implementation")
 
 # create variables
 X = {(i,j): m.addVar(vtype=gp.GRB.BINARY) for (i,j) in A} # if truck travels from i to j
-H = {(i,j,l): m.addVar(vtype=gp.GRB.BINARY) for (i,j) in A for l in L} # if drone l travels from i to j
+H = {(i,j,l): m.addVar(vtype=gp.GRB.BINARY) for i in N_s for j in N for l in L} # if drone l travels from i to j
 V = {i: m.addVar(vtype=gp.GRB.CONTINUOUS) for i in N_st} # visiting order of truck at node i
 W = {i: m.addVar(vtype=gp.GRB.CONTINUOUS) for i in N_s} # Wait time at node i
 
@@ -89,30 +93,26 @@ departFromDepot = m.addConstr(gp.quicksum(X["s",j] for j in N) == 1) # truck mus
 
 returnToDepot = m.addConstr(gp.quicksum(X[i,"t"] for i in N) == 1) # truck must return to depot
 
-# cant flow in and out of depot from same node !!! added because my subtour elim wasnt working !!!
-noFlowAlongSameArc = {(i,j): 
-    m.addConstr(X[i,j] + X[j,i] <= 1) 
-    for (i,j) in A if i != "s" and j != "t"}
-
-depotNoSameFlow = {i:
-    m.addConstr(X["s",i] + X[i,"t"] <= 1)
-    for i in N if i != "s" and i != "t"}
 
 flowConservation = {i:
-    m.addConstr(gp.quicksum(X[i,j] for j in N_t if j != i)  == gp.quicksum(X[j,i] for j in N_s if j != i))
+    m.addConstr(gp.quicksum(X[i,j] for j in N_t if j != i) == 
+                gp.quicksum(X[j,i] for j in N_s if j != i))
     for i in N}
 
 # !!! think this might be wrong !!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 subTourElimination = {(i,j): 
-    m.addConstr(V[i] - V[j] <= M*(1-X[i,j]))
+    m.addConstr(V[i] - V[j] <= M*(1-X[i,j])-1) 
     for (i,j) in A}
 
 visitEachNode = {j: 
-    m.addConstr(gp.quicksum(X[i,j] for i in N_s if i != j) + gp.quicksum(H[i,j,l] for i in N_s for l in L if i != j) == 1)
+    m.addConstr(gp.quicksum(X[i,j] for i in N_s if i != j) + 
+                gp.quicksum(H[i,j,l] for i in N_s for l in L if i != j) == 1)
     for j in N} 
 
 droneDispatch = {i:
-    m.addConstr(M*gp.quicksum(X[i,j] for j in N_t if j != i) >= gp.quicksum(H[i,j,l] for j in N for l in L if j != i))
+    m.addConstr(M*gp.quicksum(X[i,j] for j in N_t if N_t[j] != N_s[i]) >= 
+                gp.quicksum(H[i,j,l] for j in N for l in L if j != i))
     for i in N_s}
 
 batteryConsumption = {l:
@@ -123,11 +123,13 @@ waitTime = {(i,l):
     m.addConstr(W[i] >= gp.quicksum(H[i,j,l]*tau_l[i,j,l] for j in N if j != i))
     for i in N_s for l in L}
 
-m.addConstr(V["s"]==0)
+initConstr = m.addConstr(V['s']==0)
+    
+# m.addConstr(V["s"]==0)
 
 m.optimize()
 X_vals = {(i,j): X[i,j].x for (i,j) in A}
-H_vals = {(i,j,l): H[i,j,l].x for (i,j) in A for l in L}
+H_vals = {(i,j,l): H[i,j,l].x for i in N_s for j in N for l in L}
 V_vals = {i: V[i].x for i in N_st}
 
 
@@ -140,10 +142,11 @@ def plot_path(X,H):
             path.append((i,j))
     # get drone path
     drone_path = []
-    for (i,j) in A:
-        for l in L:
-            if H[i,j,l] > 0.9:
-                drone_path.append((i,j))
+    for i in N_s:
+        for j in N:
+            for l in L:
+                if H[i,j,l] > 0.9:
+                    drone_path.append((i,j))
     # plot path
     for (i,j) in path:
         plt.plot([N_st[i][0], N_st[j][0]], [N_st[i][1], N_st[j][1]], 'k')
@@ -154,6 +157,86 @@ def plot_path(X,H):
         plt.text(N_s[k][0], N_s[k][1], k, fontsize=12, horizontalalignment="right")
     plt.show()
 
+def peter_plot_path(X,H):
+    verts = []
+    for (i,j) in A:
+        if X[i,j] > 0.9:
+            #print(i,j)
+            verts.append(tuple(N_st[i]))
+            verts.append(tuple(N_st[j]))
+            
+    # print(verts)
+    codes = []
+    for i in range(int(len(verts)/2)):
+        # print(i)
+        codes.append(Path.MOVETO)
+        codes.append(Path.LINETO) 
+    # print(codes)
+    path = Path(verts, codes)
+    fig, ax = plt.subplots()
+    patch = patches.PathPatch(path, color='blue', lw=2)
+    ax.add_patch(patch)
+
+    xs, ys = zip(*verts)
+    ax.plot(xs, ys, 'x', lw=2, color='black', ms=10)
+
+    # Plot drone 0 routs
+    verts2 = []
+    for i in N_s:
+        for j in N:
+            for l in [0]:
+                if i!=j and H[i,j,l] > 0.9:
+                    # print(i,j)
+                    verts2.append(tuple(N_st[i]))
+                    verts2.append(tuple(N_st[j]))
+                    
+    # print(verts)
+    codes2 = []
+    for i in range(int(len(verts2)/2)):
+        # print(i)
+        codes2.append(Path.MOVETO)
+        codes2.append(Path.LINETO) 
+    # print(codes)
+    path2 = Path(verts2, codes2)
+    patch2 = patches.PathPatch(path2, color='orange', lw=1)
+    ax.add_patch(patch2)
+
+    xs2, ys2 = zip(*verts2)
+    ax.plot(xs2, ys2, '.', lw=2, color='orange', ms=8)
+
+    # Plot drone 1 routs
+    verts3 = []
+    for i in N_s:
+        for j in N:
+            for l in [1]:
+                if i!=j and H[i,j,l] > 0.9:
+                    # print(i,j)
+                    verts3.append(tuple(N_st[i]))
+                    verts3.append(tuple(N_st[j]))
+                    
+    # print(verts)
+    codes3 = []
+    for i in range(int(len(verts3)/2)):
+        # print(i)
+        codes3.append(Path.MOVETO)
+        codes3.append(Path.LINETO) 
+    # print(codes)
+    path3 = Path(verts3, codes3)
+    patch3 = patches.PathPatch(path3, color='red', lw=1)
+    ax.add_patch(patch3)
+
+    xs3, ys3 = zip(*verts3)
+    ax.plot(xs3, ys3, '.', lw=2, color='red', ms=8)
+
+    for n in N_s:
+        x, y = N_s[n]
+        ax.text(x-1.5,y+2, n)
+
+    ax.set_xlim(-5, 105)
+    ax.set_ylim(-5, 105)
+    # ax.set_xlim(20, 70)
+    # ax.set_ylim(20, 80)
+    plt.show()
 
 if __name__ == "__main__":
     #MIP(node_distance, drones)
@@ -166,7 +249,9 @@ if __name__ == "__main__":
     # print([X["s",j] for j in N])
     # print([X[i,"t"] for i in N])
     print(V_vals)
-    print([sum([H_vals[i,j,l]*b_l[i,j,l] for i in N_s for j in N if i != j]) for l in L]) # print out battery usage
-    print([(i,j) for (i,j) in A if X_vals[i,j] > 0.9])
-    print([(i,j,l) for (i,j) in A for l in L if H_vals[i,j,l] > 0.9])
-    plot_path(X_vals, H_vals)
+    print(f"battery usage: {[sum([H_vals[i,j,l]*b_l[i,j,l] for i in N_s for j in N if i != j]) for l in L]}") # print out battery usage
+    print(f"truck path: {[(i,j) for (i,j) in A if X_vals[i,j] > 0.9]}")
+    print(f"drone decisions{[(i,j,l) for i in N_s for j in N for l in L if H_vals[i,j,l] > 0.9]}")
+    print(f"order: {[V_vals[i] for i in V_vals]}")
+    peter_plot_path(X_vals, H_vals)
+    # Plot truck routs
